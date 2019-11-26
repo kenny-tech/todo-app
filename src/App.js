ee05919143da2e600e9db9334d5e2b5a949ae0aa
kenny-tech <kennyemma2008@gmail.com>
2019-11-26 12:50:15 +0100

    Created todo list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
+const initialTodos = [
+  {
+    id: 'a',
+    task: 'Learn Javascript',
+    complete: true,
+  },
+  {
+    id: 'b',
+    task: 'Learn React',
+    complete: true,
+  },
+  {
+    id: 'c',
+    task: 'Learn Redux',
+    complete: false,
+  }
+]
+
 const App = () => {
-  return (     
-    <p>A todo app with React hooks</p> 
+  return (   
+    <div>
+      <ul>
+        {initialTodos.map(todo => (
+          <li key={todo.id}>
+            <label>{todo.task}</label>
+          </li>
+        ))}
+      </ul>
+    </div>  
   );
 }
 
